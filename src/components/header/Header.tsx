e5a3a92157439e906ca36a2e4c9368d249fc1677
emmanuel-cabassot <emmanuel.cabassot@laplateforme.io>
2023-01-23 19:42:07 +0100

    header avec materialui

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,75 +1,245 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { AuthContext } from '../../context/authContext';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 const urlApiNest = process.env.NEXT_PUBLIC_NEXT_APP_API_URL;
 
+const pages = [{ name: 'home', url: '/' }, { name: 'Events', url: '/events' }, { name: 'About', url: '/about-us' },]
+const settings = [{ name : 'Profile', url: '/profile' }];
+
 export const Header = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+
   const { meInfos, logout, user } = useContext(AuthContext);
   useEffect(() => {
     meInfos();
   }, []);
-  
+
   const imageProfile = user && user.profileImage != null ? user.profileImage : "tux_love_windowsd6d93104-a8f5-48c7-b882-f72f204b85cb.png";
 
   return (
     <header>
-      <div>
-        <div className="topNav">
-          <Image alt="logo" src={'/favicon.ico'} width={50} height={50} />
-          <nav>
-            <ul>
-              <li>
-                <Link href="/">
-                  Home
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Link href="/">
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+            </Link>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <Link href={page.url}>
+                    <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page.name}</Typography>
+                    </MenuItem>
+                  </Link>
+                ))}
+              </Menu>
+            </Box>
+            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Link href={page.url}>
+                  <Button
+                    key={page.name}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    {page.name}
+                  </Button>
                 </Link>
-              </li>
-              <li>
-                <Link href="/events">
-                  Events
-                </Link>
-              </li>
-              
-              <li>
-                <Link href="/about-us">
-                  About us
-                </Link>
-              </li>
-              { user ? (
-                <>
-                  <li>
-                    <Link href={"/about-us"} >
-                      <Image
-                        src={`${urlApiNest}/user/profile-image/${imageProfile}`}
-                        alt={'photo'}
-                        width={20}
-                        height={20}
-                      />
-                    </Link>
-                  </li>
-                  <li>
-                    <button onClick={logout}>Logout</button>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li>
-                    <Link href="/auth/register">
-                      register
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/auth/login">
-                      login
-                    </Link>
-                  </li>
-                </>
-              )}
-            </ul>
-          </nav>
-        </div>
-        <p className="title"> Mon site</p>
-      </div>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <div>
+                    <Image src={`${urlApiNest}/user/profile-image/${imageProfile}`} alt="avatar" width={40} height={40} />
+                  <Avatar alt="avatar" sx={{ bgcolor: "white" }} src={`${urlApiNest}/user/profile-image/${imageProfile}`} />
+                  </div>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <Link href={setting.url}>
+                  <MenuItem key={setting.name} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting.name}</Typography>
+                  </MenuItem>
+                  </Link>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {/* <div>
+                <div className="topNav">
+                  <nav>
+                    <ul>
+                      <li>
+                        <Link href="/">
+                          Home
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/events">
+                          Events
+                        </Link>
+                      </li>
+
+                      <li>
+                        <Link href="/about-us">
+                          About us
+                        </Link>
+                      </li>
+                      {user ? (
+                        <>
+                          <li>
+                            <Link href={"/about-us"} >
+                              <Image
+                                src={`${urlApiNest}/user/profile-image/${imageProfile}`}
+                                alt={'photo'}
+                                width={20}
+                                height={20}
+                              />
+                            </Link>
+                          </li>
+                          <li>
+                            <button onClick={logout}>Logout</button>
+                          </li>
+                        </>
+                      ) : (
+                        <>
+                          <li>
+                            <Link href="/auth/register">
+                              register
+                            </Link>
+                          </li>
+                          <li>
+                            <Link href="/auth/login">
+                              login
+                            </Link>
+                          </li>
+                        </>
+                      )}
+                    </ul>
+                  </nav>
+                </div>
+
+              </div> */}
+
+
     </header>
   );
 };
+
+
+
